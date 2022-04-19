889e07170f5df54eb70fe3272e14444976aa7be9
JacobRWebb <xxpwnage37xx@gmail.com>
2022-04-18 22:43:44 -0500

    Frontend setup

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,8 +1,7 @@
 import Express from "express";
 
 const app = Express();
-const port = process.env.PORT || 5000;
 
 app.get("*", (req, res) => res.json({ message: "Hello World" }));
 
-app.listen(port, () => console.log(`Server started on port ${port}`));
+export default app;
---
+++ frontend/src/components/navbar/Navbar.tsx
@@ -0,0 +1,7 @@
+import { FunctionComponent } from "react";
+
+const Navbar: FunctionComponent = () => {
+  return <div>ws</div>;
+};
+
+export default Navbar;
