158a8c9cc753338483eb5a6be1a6a63a8d736e1d
JacobRWebb <xxpwnage37xx@gmail.com>
2022-04-18 22:56:33 -0500

    Testing

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -1,7 +1,12 @@
 import { FunctionComponent } from "react";
+import Navbar from "./components/navbar/Navbar";
 
 const App: FunctionComponent = () => {
-  return <div className="App"></div>;
+  return (
+    <div className="bg-black">
+      <Navbar />
+    </div>
+  );
 };
 
 export default App;
